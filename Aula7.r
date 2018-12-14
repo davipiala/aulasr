326bcd80bf5015685e61e30a67fd5bd1fd81fcab
Logon Aluno <logonrmlocal@fiap.com.br>
2018-12-13 22:00:49 -0200

    Versao aula 7 - KMEANS

--- Aula7.r
+++ Aula7.r
@@ -50,5 +50,66 @@ boxplot(fifa$dribbling  ~ fifa$preferred_foot)
 ##MAPA DE CALOR -> HEATMAP
 ##DENDROGRAMA -> SEI LAH
 
+##K MEANS -> EH MUITO ANTIGA, MAIS VELHO QUE UM COMPUTADOR
+##MUITO UTIL 
+##SUMARIZA GRANDES VOLUMES DE DADOS
+
+##PASSOS
+##DETERMINAR O NUMERO DE CLUSTER
+##DETERMINAR UM CENTROIDE PARA O CLUSTER
+##ASSOCIAR CADA UMA DA OBSERVACAO AO CENTROIDE MAIS PROXIMO
+##RECALCULAR O CENTROIDE
+
+library(plotly)
+par(mfrow=c(1,1))
+
+set.seed(1909)
+x <- rnorm(n=15, mean=rep(1:3, each=5), sd=0.2 )
+y <- rnorm(n=15, mean=rep(c(1:2), each=5), sd=0.2 )
+plot(x,y,col="blue",pch=19,cex=1)
+text(x+0.05,y+0.05, labels = as.character(1:15))
+
+mydata <- data.frame("X" <- x, "Y" <- y)
+mydata
+modelo <- kmeans(x <- mydata, centers <- 3)
+plot(x,y,col <- modelo$cluster,pch<-19, cex2 <-2)
+
+points(modelo$centers, col<- 1:3, pch <-3, cex <-3, lwd <- 2)
 
 
+
+##FILTRO
+ fifa %>% filter(league=="Spanish Primera División") %>% 
+  select (name,  acceleration, dribbling) -> spain.2d
+
+
+plot(spain.2d[,-1])
+
+
+##MODELO
+set.seed(2)
+modelo.fifa <- kmeans(x= spain.2d[,-1], centers <- 4)
+plot(spain.2d[,-1], col = modelo.fifa$cluster, pch =  21, cex= 1)
+points(modelo.fifa$centers, col=4:1 , bg=1:4, pch=24, cex=1, lwd=1)
+
+
+
+##FILTRO2
+fifa %>% filter(league=="Spanish Primera División") %>% 
+  select (name,  acceleration, dribbling, long_passing) -> spain.3d
+
+
+plot(spain.3d[,-1])
+
+
+##MODELO
+set.seed(2)
+modelo.fifa <- kmeans(x= spain.3d[,-1], centers <- 4)
+plot(spain.2d[,-1], col = modelo.fifa$cluster, pch =  21, cex= 1)
+points(modelo.fifa$centers, col=4:1 , bg=1:4, pch=24, cex=1, lwd=1)
+
+
+##FILTRO
+fifa$league
+liga_espanha_ <- fifa %>% filter(league=="Spanish Primera División") %>% 
+  select ( acceleration, dribbling, long_passing)
